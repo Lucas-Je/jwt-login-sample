3b0d52ae6d10373ee7656e7c556d371f686f0cd4
Lucas <kid9night@naver.com>
2023-03-15 21:08:50 +0900

    test: add some test code
     - github action 테스트

--- src/constnats/login.js
+++ src/constnats/login.js
@@ -1,4 +1,3 @@
 export const COOKIES = {
     REFRESH_TOKEN: 'refresh-token',
-    TEST: 'test',
 };
--- src/repository/APIs.js
+++ src/repository/APIs.js
@@ -4,7 +4,6 @@ const url = {
     auth: {
         login: '/api/v1/auth/signin',
         refreshToken: '/api/v1/auth/refresh',
-        testapi: '/api/v1/auth/test',
     },
 };
 
