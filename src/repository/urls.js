fc678ea35eeb441bc0afa000d92db4b126b04831
Lucas <lucas@hyperhire.in>
2023-03-16 14:59:00 +0900

    test: add some test code
     - github action 테스트

--- src/repository/urls.js
+++ src/repository/urls.js
@@ -2,6 +2,6 @@ export const api = {
     auth: {
         login: '/api/v1/auth/login',
         reIssueToken: '/api/v1/auth/reissue-token',
-        reIssueToken1: '/api/v1/auth/reissue-token1',
+        reIssueToken2: '/api/v1/auth/reissue-token2',
     },
 };
