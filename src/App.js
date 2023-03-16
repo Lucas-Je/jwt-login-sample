99df3a4d689dcf530aecf29b48008a7c088e6e6b
Lucas <lucas@hyperhire.in>
2023-03-16 15:33:26 +0900

    test: add some test code
     - github action 테스트

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         Logger.log(data)
     }
     const onSubmit = async formData => {
-        console.log("onSubmit3", formData)
+        console.log("onSubmit", formData)
         login(formData);
     };
   return (
